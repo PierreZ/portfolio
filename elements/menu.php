269c3722ae3e1614f3b3b639fe2a46b30ebc354e
Pierre Zemb <pierre.zemb.isen@gmail.com>
2014-03-02 22:07:52 +0100

    adding welcome part

---
+++ elements/menu.php
@@ -0,0 +1,24 @@
+<ul id="gn-menu" class="gn-menu-main">
+    <li class="gn-trigger">
+        <a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+        <nav class="gn-menu-wrapper">
+        	<div class="gn-scroller">
+              <ul class="gn-menu">
+                  <li><a  href="/" class="gn-icon gn-icon-home">Home</a></li>
+                  <li><a href="http://blog.pierrezemb.fr" class="gn-icon gn-icon-blog" >Blog</a></li>        		
+                  <li><a href="/work" class="gn-icon gn-icon-work" >Work experience and education</a></li>
+                  <li><a href="/skills" class="gn-icon gn-icon-skills" >Skills</a></li>   
+                  <li><a href="/projects/" class="gn-icon gn-icon-code">Projects</a></li>
+                  <li><a href="/CV.pdf"class="gn-icon gn-icon-profile">Download my resume</a></li>
+                  <li><a href="/gallery" class="gn-icon gn-icon-images">Gallery and movies</a></li>
+              </ul>
+              
+          </div><!-- /gn-scroller -->
+      </nav>
+      <li><div class="logo-isen">
+          <h1>ISEN</h1>
+          <h2>École d'ingénieurs</h2>
+      </div></li>
+  </ul>
+
+
