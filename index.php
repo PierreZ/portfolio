66ec24b08d29435a8d19c527281a8a4b9e2fd788
PierreZ <pierre.zemb.isen@gmail.com>
2014-03-07 09:46:02 +0100

    adding slideup and down

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 require("elements/menu.php");
 ?> 
 
- <div class="row welcome">
+<div class="row welcome">
 
   <h1>Pierre Zemb<br/>
     <small>
@@ -46,150 +46,157 @@ require("elements/menu.php");
     </small>
   </h1>
   <div class="social">
-        <a  href="http://www.facebook.com/pierre.zemb"><img class="socials_icons" src="img/facebook.png" alt="Facebook" /></a>
-        <a href="http://twitter.com/PierreZ"><img class="socials_icons" src="img/twitter.png" alt="Twitter" /></a>
-        <a href="http://plus.google.com/108156342712745521162"><img class="socials_icons" src="img/googleplus-revised.png" alt="Google+" /></a>
-        <a href="http://www.flickr.com/photos/34207436@N03/sets/"><img class="socials_icons" src="img/flickr.png" alt="Flickr" /></a>
-        <a href="http://www.linkedin.com/pub/pierre-zemb/5b/412/800"><img class="socials_icons" src="img/linkedin.png" alt="Youtube" /></a>
-        <a href="https://github.com/PierreZ"><img class="socials_icons"  src="img/github.png" alt="git" /></a>
+    <a  href="http://www.facebook.com/pierre.zemb"><img class="socials_icons" src="img/facebook.png" alt="Facebook" /></a>
+    <a href="http://twitter.com/PierreZ"><img class="socials_icons" src="img/twitter.png" alt="Twitter" /></a>
+    <a href="http://plus.google.com/108156342712745521162"><img class="socials_icons" src="img/googleplus-revised.png" alt="Google+" /></a>
+    <a href="http://www.flickr.com/photos/34207436@N03/sets/"><img class="socials_icons" src="img/flickr.png" alt="Flickr" /></a>
+    <a href="http://www.linkedin.com/pub/pierre-zemb/5b/412/800"><img class="socials_icons" src="img/linkedin.png" alt="Youtube" /></a>
+    <a href="https://github.com/PierreZ"><img class="socials_icons"  src="img/github.png" alt="git" /></a>
   </div>
 
 </div>
 
 
 
-    <div class="row about">
-      <h2 class="text-center">About Me</h2>
-      <div class="google_color text-center"></div>
-      <div class="text-center col-md-3">
-        <img class="profile_image" src="img/myself.png">
-      </div>
-      <div class="col-md-9">
-        <p>
-          Hi! My name's Pierre Zemb, and I'm a <strong>french student</strong> in an engineering school called <strong>ISEN </strong>where I'm currently studying in the Computer and Network Cycle.
-        </p>
-        <p>
-          I first became interested in development during my training, and I never stop ever since. Thanks to it, I learned thorough computer skills.<strong>I am really enjoying learning new languages or frameworks by myself</strong>, and using them to <strong>develop either web or native apps on my free time</strong>. I'm really enjoying coding in <strong>Java and C++</strong>, but <strong>I always end up developping web apps</strong>.
-        </p>
-        <p>
-          I am committed to many local meetings such as <strong>Google Developers Group, Java User Group or Startup Week-end</strong>. I am also the vice-president of the IT-Electronics club of ISEN, where I became a <strong>new-tech speaker among other things</strong>.
-        </p>
-        <p>
-          As a work experience, I spent <strong>a year in a part-time internship as a DevOps</strong>(developer and sysadmin)  at Systems team for a French bank, <strong>where I developed scripts and apps</strong> around a Realtime Monitoring Infrastructure which involves technology like <strong>Hadoop, Flume, Kafka, Zookeeper, or ElasticSearch…</strong>
-        </p>
-         
-      </div>
-       <p class="text-center"><a href="/CV.pdf">Download my resume</a></li></p>
+<div class="row about">
+  <h2 class="text-center">About Me</h2>
+  <div class="google_color text-center"></div>
+  <div class="text-center col-md-3">
+    <img class="profile_image" src="img/myself.png">
+  </div>
+  <div class="col-md-9">
+    <p>
+      Hi! My name's Pierre Zemb, and I'm a <strong>french student</strong> in an engineering school called <strong>ISEN </strong>where I'm currently studying in the Computer and Network Cycle.
+    </p>
+    <p>
+      I first became interested in development during my training, and I never stop ever since. Thanks to it, I learned thorough computer skills.<strong>I am really enjoying learning new languages or frameworks by myself</strong>, and using them to <strong>develop either web or native apps on my free time</strong>. I'm really enjoying coding in <strong>Java and C++</strong>, but <strong>I always end up developping web apps</strong>.
+    </p>
+    <p>
+      I am committed to many local meetings such as <strong>Google Developers Group, Java User Group or Startup Week-end</strong>. I am also the vice-president of the IT-Electronics club of ISEN, where I became a <strong>new-tech speaker among other things</strong>.
+    </p>
+    <p>
+      As a work experience, I spent <strong>a year in a part-time internship as a DevOps</strong>(developer and sysadmin)  at Systems team for a French bank, <strong>where I developed scripts and apps</strong> around a Realtime Monitoring Infrastructure which involves technology like <strong>Hadoop, Flume, Kafka, Zookeeper, or ElasticSearch…</strong>
+    </p>
 
-        <p class="text-center"><a href="/CV_fr.pdf" class="lang">Mon CV français est dispo ici !</a></p>
+  </div>
+  <p class="text-center"><a href="/CV.pdf">Download my resume</a></li></p>
 
-    </div>
+  <p class="text-center"><a href="/CV_fr.pdf" class="lang">Mon CV français est dispo ici !</a></p>
+
+</div>
 
 <!-- ng-if="skill.type=='skillSelected'||skillSelected=='All'" 
- -->  <div class="row skills">
-      <h2 class="text-center">Skills</h2>
-      <div class="google_color text-center"></div>
-      <div class="container">
-        <ul class="nav nav-pills nav-justified skills-nav text-center">
-          <li ng-click="setSkill(cat)" ng-class="{active:skillSelected==cat}"
-          ng-repeat="cat in skills_cat"><a href="">{{cat}}</a></li>
+-->  <div class="row skills">
+<h2 class="text-center">Skills</h2>
+<div class="google_color text-center"></div>
+<div class="container">
+  <ul class="nav nav-pills nav-justified skills-nav text-center">
+    <li ng-click="setSkill(cat)" ng-class="{active:skillSelected==cat}"
+    ng-repeat="cat in skills_cat"><a href="">{{cat}}</a></li>
+  </ul>
+</div>
+<dl class="skills-diagram">
+  <dt ng-repeat="skill in skills | filter:skill_filter" ng-class="skill.css">{{skill.name}}</dt>
+  <dd>0</dd>
+</dl>
+</div>
+
+<div class="row work">
+  <h2 class="text-center">Work experience</h2>
+  <div class="google_color text-center"></div>
+  <div class="col-md-4">
+    <article>
+      <header>
+        <h3 class="text-center arkea_trigger">Crédit Mutuel Arkéa</h3>
+        <a href="http://www.cmarkea.com/"><img src="img/arkea.png" class="md-4-img"></a>
+        <p class="arkea_trigger"><strong>Internship </strong> June-July 2013</p>
+        <p class="arkea_trigger"><strong>Part-time internship </strong> October 2013-September 2014</p>
+      </header>
+      <div class="arkea_detail">
+        <p><em>"Analysis and implementation of technical metrics on VMware virtualized environments and Windows servers"</em></p>
+        <p>I worked as a <strong>DevOps</strong>(developer and sysadmin) at Systems team, where I developed scripts and apps around a Realtime Monitoring Infrastructure which involves technology like<strong> Hadoop, Flume, Kafka, Zookeeper, or ElasticSearch</strong>…</p>
+        <p><strong>Some of my work:</strong></p>
+        <ul>
+          <li>Merging and upgrading Arkea's version of Apache Flume (Maven Project)</li>
+          <li>Porting arkea's version of Flume for Windows</li>
+          <li>Writing some VBscripts to generating Windows's metrics</li>
+          <li>Writing Powershell scripts to retrieve VMWare metrics</li>
+          <li>In-house training about Hadoop and Pig</li>
+          <li>Creation of an web-based app to visualize metrics in realtime using AngularJS</li>
         </ul>
       </div>
-      <dl class="skills-diagram">
-      <dt ng-repeat="skill in skills | filter:skill_filter" ng-class="skill.css">{{skill.name}}</dt>
-        <dd>0</dd>
-      </dl>
+    </article>
   </div>
-
-    <div class="row work">
-      <h2 class="text-center">Work experience</h2>
-      <div class="google_color text-center"></div>
-      <div class="col-md-4">
-        <article>
-          <header>
-            <h3 class="text-center">Crédit Mutuel Arkéa</h3>
-            <a href="http://www.cmarkea.com/"><img src="img/arkea.png" class="md-4-img"></a>
-            <p><strong>Devops internship </strong> June-July 2013</p>
-            <p><strong>Devops part-time internship </strong> October 2013-September 2014</p>
-          </header>
-          <p><em>"Analysis and implementation of technical metrics on VMware virtualized environments and Windows servers"</em></p>
-          <p>I worked as a <strong>DevOps</strong>(developer and sysadmin) at Systems team, where I developed scripts and apps around a Realtime Monitoring Infrastructure which involves technology like<strong> Hadoop, Flume, Kafka, Zookeeper, or ElasticSearch</strong>…</p>
-          <p><strong>Some of my work:</strong></p>
-          <ul>
-            <li>Merging and upgrading Arkea's version of Apache Flume (Maven Project)</li>
-            <li>Porting arkea's version of Flume for Windows</li>
-            <li>Writing some VBscripts to generating Windows's metrics</li>
-            <li>Writing Powershell scripts to retrieve VMWare metrics</li>
-            <li>In-house training about Hadoop and Pig</li>
-            <li>Creation of an web-based app to visualize metrics in realtime using AngularJS</li>
-
-          </ul>
-        </article>
+  <div class="col-md-4">
+    <article>
+      <header>
+        <h3 class="text-center thomson_trigger">Thomson Video Networks</h3>
+        <a href="https://www.thomson-networks.com"><img src="img/thomson.png" class="md-4-img"></a>
+        <p class="thomson_trigger"><strong>developer internship </strong> June-July 2012</p>
+      </header>
+      <div class="thomson_detail">
+        <p>I worked at the <strong>Headquarters</strong> of Thomson Video Networks on the Configurator. It's a <strong>really heavy Excel program</strong> allows the employees to calculate the price list of every product, knowing that they are extremely flexible. One click was representing <strong>minutes of wait</strong> just to calculate what's possible to add.</p>
+        <p>My goal was to <strong>optimize it</strong> to make it more useable.</p>
       </div>
-      <div class="col-md-4">
-        <article>
-          <header>
-            <h3 class="text-center">Thomson Video Networks</h3>
-            <a href="https://www.thomson-networks.com"><img src="img/thomson.png" class="md-4-img"></a>
-            <p><strong>developer internship </strong> June-July 2012</p>
-          </header>
-          <p><em></em></p>
-          <p>I worked at the <strong>Headquarters</strong> of Thomson Video Networks on the Configurator. It's a <strong>really heavy Excel program</strong> allows the employees to calculate the price list of every product, knowing that they are extremely flexible. One click was representing <strong>minutes of wait</strong> just to calculate what's possible to add.</p>
-          <p>My goal was to <strong>optimize it</strong> to make it more useable.</p>
-        </article>
-      </div>
-      <div class="col-md-4">
-        <article>
-          <header>
-            <h3 class="text-center">CAEC</h3>
-            <a href="http://caec.pagesperso-orange.fr/page13.html"><img src="img/caec.gif" class="md-4-img"></a>
-            <p><strong>Charity work </strong>July 2012</p>
-          </header>
-          <p><strong>Assistance for unemployed</strong> to help them find a job</p>
-        </article>
-      </div>
-    </div>
+    </article>
+  </div>
+  <div class="col-md-4">
+    <article>
+      <header>
+        <h3 class="text-center">CAEC</h3>
+        <a href="http://caec.pagesperso-orange.fr/page13.html"><img src="img/caec.gif" class="md-4-img"></a>
+        <p><strong>Charity work </strong>July 2011</p>
+      </header>
+      <p><strong>Assistance for unemployed</strong> to help them find a job</p>
+    </article>
+  </div>
+</div>
 
 
-  <div class="row education">
-    <h2 class="text-center">Education</h2>
-    <div class="google_color text-center"></div>
-    <div class="col-md-6">
-      <article>
-        <header class="text-center">
-          <h3>Institut Supérieur de l'Electronique et du Numérique</h3>
-          <a href="http://isen.fr"><div class="logo-isen">
-            <h1>ISEN</h1>
-            <h2>École d'ingénieurs</h2>
-          </div></a>
-          <p>French engineering school - <strong>Estimate graduation:</strong> 2016</p>
-        </header>
-        <h4 class="text-center"><strong>Computer and Network Cycle(2 years)</strong></h4>
+<div class="row education">
+  <h2 class="text-center">Education</h2>
+  <div class="google_color text-center"></div>
+  <div class="col-md-6">
+    <article>
+      <header class="text-center">
+        <h3>Institut Supérieur de l'Electronique et du Numérique</h3>
+        <a href="http://isen.fr"><div class="logo-isen">
+          <h1>ISEN</h1>
+          <h2>École d'ingénieurs</h2>
+        </div></a>
+        <p>French engineering school - <strong>Estimate graduation:</strong> 2016</p>
+      </header>
+      <h4 class="text-center CIR_trigger"><strong>Computer and Network Cycle(2 years)</strong></h4>
+      <div class="CIR_detail">
         <p>After spending 2 years in preparatory classes, I've decided to go into <strong>Computer and Cycle</strong> in order to enhanced my technical skills.</p>
-<p><strong>Technical courses:</strong></p>
-<ul>
-<li><strong> Hardware</strong></li>
-<li><strong> Linux-based systemes</strong></li>
-<li><strong> Cisco-based courses: Network Basics/Routing and Switching/Scaling Networks(CCNA 1-2-3-4)</strong></li>
-<li><strong> Databases</strong></li>
-<li><strong> C, C++, Java</strong></li>
-<li><strong> PHP, Javascript, HTML5, CSS3</strong></li>
-</ul>
-<h4 class="text-center"><strong>Preparatory classes(2 years)</strong></h4>
-<p>They consist of <strong>two very intensive years</strong> which act as a preparatory course with the main goal of training undergraduate students.<br/>The workload is one of the<strong> highest in the world</strong>(between 35 and 40 contact hours a week, including written and oral exams).</p>
-      </article>
-    </div>
-    <div class="col-md-6 text-center">
-      <h2>More to come!</h2>
-      <p>My goal is to spend a full year in a foreign country during my final year at ISEN!</p>
-    </div>
+        <p><strong>Technical courses:</strong></p>
+        <ul>
+          <li><strong> Hardware</strong></li>
+          <li><strong> Linux-based systemes</strong></li>
+          <li><strong> Cisco-based courses: Network Basics/Routing and Switching/Scaling Networks(CCNA 1-2-3-4)</strong></li>
+          <li><strong> Databases</strong></li>
+          <li><strong> C, C++, Java</strong></li>
+          <li><strong> PHP, Javascript, HTML5, CSS3</strong></li>
+        </ul>
+      </div>
+        <h4 class="text-center prepa_trigger"><strong>Preparatory classes(2 years)</strong></h4>
+      <div class="prepa_detail">
+        <p>They consist of <strong>two very intensive years</strong> which act as a preparatory course with the main goal of training undergraduate students.<br/>The workload is one of the<strong> highest in the world</strong>(between 35 and 40 contact hours a week, including written and oral exams).</p>
+      </div>
+    </article>
+
   </div>
+  <div class="col-md-6 text-center">
+    <h2>More to come!</h2>
+    <p>My goal is to spend a full year in a foreign country during my final year at ISEN!</p>
+  </div>
+</div>
 
-    <hr>
+<hr>
 
-  <footer>
-    <p class="text-center">&copy; Pierre Zemb 2014</p>
-  </footer>
+<footer>
+  <p class="text-center">&copy; Pierre Zemb 2014</p>
+</footer>
 </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+$( document ).ready(function() {
+
+	//CIR
+    $(".CIR_detail").hide();
+    $( ".CIR_trigger" ).click(function() {
+    	$(".CIR_detail").slideDown("slow");
+	});
+    $( ".CIR_detail" ).click(function() {
+    	$(".CIR_detail").slideUp("slow");
+	});
+    //Prepa
+	$(".prepa_detail").hide();
+    $( ".prepa_trigger" ).click(function() {
+    	$(".prepa_detail").slideDown("slow");
+	});
+    $( ".prepa_detail" ).click(function() {
+    	$(".prepa_detail").slideUp("slow");
+	});
+    //arkea
+	$(".arkea_detail").hide();
+    $( ".arkea_trigger" ).click(function() {
+    	$(".arkea_detail").slideDown("slow");
+	});
+    $( ".arkea_detail" ).click(function() {
+    	$(".arkea_detail").slideUp("slow");
+	});
+    //thomson
+	$(".thomson_detail").hide();
+    $( ".thomson_trigger" ).click(function() {
+    	$(".thomson_detail").slideDown("slow");
+	});
+    $( ".thomson_detail" ).click(function() {
+    	$(".thomson_detail").slideUp("slow");
+	});
+});
+
+
 var myApp = angular.module('PortfolioApp', ['ngAnimate']);
 myApp.controller('Ctrl', function ($scope) {
 	$scope.skills=shuffle(skills);
